e146aa67aa2bb5ed68e7123bc372c6b828ab2bbd
Serg Vasylchak <hustle3942@gmail.com>
2022-07-27 20:14:45 +0300

    Revert "Feature/create account"

--- src/main/java/com/eleks/academy/whoami/WhoAmIApplication.java
+++ src/main/java/com/eleks/academy/whoami/WhoAmIApplication.java
@@ -3,14 +3,11 @@ package com.eleks.academy.whoami;
 import com.eleks.academy.whoami.api.GameApi;
 import com.eleks.academy.whoami.handler.ApiClient;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
 
-
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 @ComponentScan(basePackages = {"com.eleks.academy.whoami"}, excludeFilters = {
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = ApiClient.class),
         @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, value = GameApi.class)
--- src/main/java/com/eleks/academy/whoami/core/exception/UserAlreadyExistsException.java
+++
@@ -1,8 +0,0 @@
-package com.eleks.academy.whoami.core.exception;
-
-public class UserAlreadyExistsException extends RuntimeException{
-
-    public UserAlreadyExistsException(String message) {
-        super(message);
-    }
-}
